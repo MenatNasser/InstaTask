507bb1fd23808cd4e93c033295661ec2aad9fa6d
menat-allah <menatallah.nasser@tajawal.com>
2019-05-08 21:41:52 +0200

    Adjusting Test cases

--- src/test/java/com/instabug/AppTest.java
+++ src/test/java/com/instabug/AppTest.java
@@ -2,7 +2,6 @@ package com.instabug;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,17 +9,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Automated test cases for Login page.
  */
 public class AppTest {
     /**
@@ -28,48 +22,60 @@ public class AppTest {
      */
     private WebDriver driver;
 
+    // This is to setup testing execution before each test case
     @Before
-    public void setup() {
+    public void setupTestExec() {
 
-        //close browser
+        //Closing any open webdriver opens
+//          if (driver.getWindowHandles().isEmpty())
+//        {
+//            System.out.println("New driver will be initiated");
+//        }
+//          else
+//          {
+//              driver.quit();
+//          }
 
+
+        //Initiating new driver for test cases
         driver = new ChromeDriver();
 
-//        driver.get("https://instabug.com");
-//        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-//        WebElement link= driver.findElements(By.linkText("Login")).get(0);
-//        WebElement element = driver.findElements(By.cssSelector("a[href=https://dashboard.instabug.com/login]")).get(0);
-//        link.click();
-
+        //Enter login  page url to use in executing test cases
         driver.get("https://dashboard.instabug.com/login");
-        //logout
 
     }
 
-    // write login successful
+    // First case: Successful login using correct credentials
     @Test
     public void successFullLoginWithCredentialsCase() {
-        try {
 
+        //Checking availability of web elements to enter credentials values in it
+        try {
+            //Email field
             WebElement emailField = driver.findElement(By.id("developer_email"));
             emailField.sendKeys("mennafortesting@gmail.com");
 
+            //Password field
             WebElement PasswordField = driver.findElement(By.id("password"));
             PasswordField.sendKeys("menna_1991");
 
+            // Login button
             WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
             loginButton.click();
 
-        } catch (NoSuchElementException e) {
+        }
+        // Catching exception in case any of fields no existing
+        catch (NoSuchElementException e) {
             fail();
         }
 
-
+        //Verifying successful login
         try {
-            //detecting login validation
+            //Wait time till login page loads successfully
             WebDriverWait wait = new WebDriverWait(driver, 10);
             wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("c-spark-avatar")));
 
+            //Asserting on Profile logo and toggle list after login
             WebElement validatingLogin = driver.findElement(By.className("c-spark-avatar"));
             assertTrue(validatingLogin.isDisplayed());
 
@@ -80,105 +86,121 @@ public class AppTest {
             WebDriverWait wait2 = new WebDriverWait(driver, 10);
             wait2.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//a[@ng-click='topNavbarMenuVm.onLogout()']")));
             WebElement logoutElement = driver.findElement(By.xpath("//a[@ng-click='topNavbarMenuVm.onLogout()']"));
-            System.out.println(logoutElement.getTagName());
-            System.out.println(logoutElement);
             logoutElement.click();
+        }
 
-
-        } catch (TimeoutException e) {
+        //In case of failed login page load
+        catch (TimeoutException e) {
             System.out.println("TimeOut");
             fail();
         }
-
-
     }
 
 
-    // Both Email and Password Fields are blank.
+    // Second case: Both Email and Password Fields are blank.
     @Test
     public void blankFieldsCase() {
 
-        WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
-        if (loginButton.isEnabled()) {
-            loginButton.click();
-        } else {
-            System.out.println("Can't leave fields empty");
+        //Checking that login button can't be enabled with empty fields for email and password
+        try {
+            // Login button
+            WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
+            assertFalse(loginButton.isEnabled());
         }
-
+        // Catching exception in case any of fields no existing
+        catch (NoSuchElementException e) {
+            fail();
+        }
     }
 
 
-    //Email field is filled and Password field is blank
+    // Third case: Email field is filled and Password field is blank
     @Test
-    public void emailOnlyfilledCase() {
-
-        WebElement emailField = driver.findElement(By.id("developer_email"));
-        if (emailField.isDisplayed()) {
+    public void emailOnlyEnteredCase() {
+        //Entering value to email only
+        try {
+            //Email field
+            WebElement emailField = driver.findElement(By.id("developer_email"));
             emailField.sendKeys("mennafortesting@gmail.com");
-        } else {
-            System.out.println("email field doesn't exists");
+
+            //Password field
+            WebElement PasswordField = driver.findElement(By.id("password"));
+            PasswordField.sendKeys("");
+
+            // Login button
+            WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
+            assertFalse(loginButton.isEnabled());
         }
-        WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
-        if (loginButton.isEnabled()) {
-            loginButton.click();
-        } else {
-            System.out.println("Can't password fields empty");
+
+        // Catching exception in case any of fields no existing
+        catch (NoSuchElementException e) {
+            fail();
         }
     }
 
-    //Password field is filled and email field is blank
+    // Fourth case: Password field is filled and email field is blank
     @Test
     public void passwordOverfilledCase() {
+        //Entering value to password only
+        try {
+            //Email field
+            WebElement emailField = driver.findElement(By.id("developer_email"));
+            emailField.sendKeys("");
 
-        WebElement PasswordField = driver.findElement(By.id("password"));
-        if (PasswordField.isDisplayed()) {
+            //Password field
+            WebElement PasswordField = driver.findElement(By.id("password"));
             PasswordField.sendKeys("menna_1991");
-        } else {
 
+            // Login button
             WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
-            if (loginButton.isEnabled()) {
-                loginButton.click();
-            } else {
-                System.out.println("Can't email fields empty");
-            }
+            assertFalse(loginButton.isEnabled());
+        }
+
+        // Catching exception in case any of fields no existing
+        catch (NoSuchElementException e) {
+            fail();
         }
     }
 
-    //Email and/or Password are entered wrong
+    // Fifth case: Email and/or Password are entered wrong
     @Test
     public void wrongCredentialsCase() {
-
-        WebElement emailField = driver.findElement(By.id("developer_email"));
-        emailField.sendKeys("me@test.com");
-        assertTrue(emailField.isDisplayed());
-
-        WebElement PasswordField = driver.findElement(By.id("password"));
-        PasswordField.sendKeys("mnoeee_1991");
-        assertTrue(PasswordField.isDisplayed());
-
-        WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
-        loginButton.click();
-
-
         try {
+            //Email field entered with wrong value
+            WebElement emailField = driver.findElement(By.id("developer_email"));
+            emailField.sendKeys("mennafortting@gmail.com");
 
+            //Password field entered with wrong value
+            WebElement PasswordField = driver.findElement(By.id("password"));
+            PasswordField.sendKeys("menna1991");
+
+            // Login button
+            WebElement loginButton = driver.findElement(By.xpath("//*[@class='c-button c-button--info c-button--block c-button--xlarge u-bottom-margin--1x']"));
+            assertTrue(loginButton.isEnabled());
+            loginButton.click();
+        }
+        // Catching exception in case any of fields no existing
+        catch (NoSuchElementException e) {
+            fail();
+        }
+        //Asserting on error message returns in case of any wrong credentials
+        try {
+            //Wait time till error message appears
             WebDriverWait wait = new WebDriverWait(driver, 2);
             wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@ng-show='developerLoginVm.invalidMessage']/p")));
 
             WebElement wrongCredErrorMsg = driver.findElement(By.xpath("//div[@ng-show='developerLoginVm.invalidMessage']/p"));
             assertTrue(wrongCredErrorMsg.isDisplayed());
-
-        } catch (TimeoutException e) {
+        }
+        catch (TimeoutException e) {
             System.out.println("TimeOut");
             fail();
         }
-
     }
-
 
     @After
     public void close() {
-        //driver.close();
-        //driver.quit();
+        driver.close();
+        driver.quit();
     }
 }
